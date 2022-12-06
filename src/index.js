d9f11ad37b56598b3e32e6dfd76c65db77c6fcb9
Samueelx <gsammygitau@gmail.com>
2022-12-06 14:56:21 +0300

    Completed challenge.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const button = document.querySelector('#create-task-form :nth-child(3)');
+  const input = document.querySelector('#create-task-form :nth-child(2)');
+  const ul = document.querySelector('#tasks');
+  
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(input.value !== ""){
+      const li = document.createElement('li');
+      li.textContent = input.value;
+      ul.append(li);
+      input.value = "";
+    } else {
+      alert("Please input task!");
+    }
+  });
 });
